dcb93e20ac2ed46c6e14672f7827be7e48bde854
Jitendra Kumar <31926693+Jitendra2239@users.noreply.github.com>
2025-11-08 21:15:57 +0530

    controller added

---
+++ src/main/java/com/example/paymentservice/DemoController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello from DemoController 🚀";
+    }
+
+    @GetMapping("/status")
+    public String status() {
+        return "Application is running successfully ✅";
+    }
+}
